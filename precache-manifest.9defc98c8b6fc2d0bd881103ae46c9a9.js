edf65fac0a34f283ad43039d6870ac1c40cb64fc
unknown <m.y.owyeong98@gmail.com>
2020-10-09 11:07:17 +0800

    Updates

---
+++ precache-manifest.9defc98c8b6fc2d0bd881103ae46c9a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348ed31b742d7ce370a24f5c840d33ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8c90c36e869415ede1e",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "40e776ab05397ca4af29",
+    "url": "/static/css/main.7b1d56c8.chunk.css"
+  },
+  {
+    "revision": "a8c90c36e869415ede1e",
+    "url": "/static/js/2.8cd91779.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.8cd91779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e776ab05397ca4af29",
+    "url": "/static/js/main.59726e51.chunk.js"
+  },
+  {
+    "revision": "f9a972af0f782e3cce40",
+    "url": "/static/js/runtime-main.b00022e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
